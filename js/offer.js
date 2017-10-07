64c2b94def394c0e082ede8d4d96d7736871166b
Vyacheslav <thirtylynx@gmail.com>
2017-10-07 14:22:25 +0500

    Улучшил вид скрипта offer

--- js/offer.js
+++ js/offer.js
@@ -1,8 +1,26 @@
+'use strict';
+
 (function () {
+  var KEY_CODES = {
+    ESC: 27
+  };
   var field = document.querySelector("#catalogg");
   var popup = document.querySelector(".modal-content");
-  var inputName = document.querySelector("#names");
+  var inputName = document.querySelector("#product");
   var overlay = document.querySelector(".modal-overlay");
+
+  var closePopup = function (event) {
+    if (event.keyCode === KEY_CODES.ESC) {
+      if (popup.classList.contains("modal-content--show")) {
+        popup.classList.remove("modal-content--show");
+        overlay.classList.remove("modal-overlay--show");
+      }
+    } else {
+      event.preventDefault();
+      popup.classList.remove("modal-content--show");
+      overlay.classList.remove("modal-overlay--show");
+    }
+  };
 
   field.onclick = function (event) {
     var target = event.target;
@@ -12,19 +30,6 @@
       overlay.classList.add("modal-overlay--show");
     }
   };
-
-  window.addEventListener("keydown", function (event) {
-    if (event.keyCode === 27) {
-      if (popup.classList.contains("modal-content--show")) {
-        popup.classList.remove("modal-content--show");
-        overlay.classList.remove("modal-overlay--show");
-      }
-    }
-  });
-  overlay.addEventListener("click", function (event) {
-    event.preventDefault();
-    popup.classList.remove("modal-content--show");
-    overlay.classList.remove("modal-overlay--show");
-  });
-
-}) ();
+  document.addEventListener("keydown", closePopup);
+  overlay.addEventListener("click", closePopup);
+})();
